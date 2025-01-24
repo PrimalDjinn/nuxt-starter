806e0449a949662b9961ca8b17d1488914ac2c9d
kgarchie <archiethebig@gmail.com>
2025-01-24 19:02:44 +0300

    state

--- app/composables/state.ts
+++ app/composables/state.ts
@@ -2,45 +2,94 @@ import consola from "consola";
 import { isDevelopment } from "../../server/utils/env";
 
 type Result<T> = T | undefined;
-type Error = any | undefined;
-type Return<T> = { data: Ref<Result<T>>; error: Error };
-
-export const useAsyncState = async <T>(
+type Error = Ref<any | undefined>;
+export async function useAsyncState<T>(
   key: string,
-  fn: () => Promise<T>
-): Promise<Return<T>> => {
+  fn: () => Promise<Result<T>>
+): Promise<{ data: Ref<Result<T>>; error: Error }>;
+export async function useAsyncState<T>(
+  key: string,
+  fn: () => Promise<Result<T>>,
+  options: { immediate: true }
+): Promise<{ data: Ref<Result<T>>; error: Error }>;
+export async function useAsyncState<T>(
+  key: string,
+  fn: () => Promise<Result<T>>,
+  options: { immediate: false }
+): Promise<{
+  data: Ref<Result<T>>;
+  error: Error;
+  execute: () => Promise<void>;
+}>;
+export async function useAsyncState<T>(
+  key: string,
+  fn: () => Promise<Result<T>>,
+  options: { immediate: boolean }
+): Promise<{
+  data: Ref<Result<T>>;
+  error: Error;
+  execute?: () => Promise<void>;
+}>;
+export async function useAsyncState<T>(
+  key: string,
+  fn: () => Promise<Result<T>>,
+  options?: { immediate?: boolean }
+): Promise<{
+  data: Ref<Result<T>>;
+  error: Error;
+  execute?: () => Promise<void>;
+}> {
+  if (options?.immediate !== false) {
+    options = { immediate: true };
+  }
   async function pull() {
     try {
       const data = await fn();
-      return { result: data, error: undefined };
+      return { data, error: undefined };
     } catch (error) {
-      return Promise.resolve({ error: error as Error, result: undefined });
+      return Promise.resolve({ error: error as Error, data: undefined });
     }
   }
 
+  let initial: Ref<Result<T>>;
+  const error = ref<Error | undefined>(undefined);
   if (tryUseNuxtApp()) {
-    const { data: initial } = useNuxtData(key);
-    if (initial.value)
-      return Promise.resolve({ data: initial as Ref<T>, error: undefined });
-
-    const { result, error } = await pull();
-    initial.value = result;
-    if (error) {
-      consola.error("An error occurred while fetching data for", key);
-      consola.error(error);
-    }
-    return { data: initial, error };
+    const { data } = useNuxtData(key);
+    initial = data;
   } else {
     if (isDevelopment) {
-      consola.warn(
+      consola.info(
         "Nuxt instance not found, re-executing initialisation function and discarding results right after." +
           " There is no state thus, results will not be re-used, please check your implementation." +
           " This has a low likelihood of being a Nuxt error, well unless you are an idiot, you idiot."
       );
     }
-
-    const { result, error } = await pull();
-    consola.log("result", result, error);
-    return { data: ref(result) as Ref<typeof result>, error };
+    initial = ref(undefined);
   }
-};
+
+  async function execute() {
+    const { data: _data, error: _error } = await pull();
+    initial.value = _data;
+    if (_error) {
+      consola.error("An error occurred while fetching data for", key);
+      consola.error(error.value);
+      error.value = _error;
+
+      if (
+        typeof error?.value === "string" &&
+        error.value.toLocaleLowerCase().includes("nuxt")
+      ) {
+        console.warn(
+          "Please note that the initialisation function of useAsyncState should not contain a function that needs nuxt instance"
+        );
+      }
+    }
+  }
+
+  if (options.immediate) {
+    if (!initial.value) await execute();
+    return { data: initial, error };
+  }
+
+  return Promise.resolve({ data: initial, error, execute });
+}
