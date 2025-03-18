9b96136f195f3399249bfbfd246147297bc786ee
PrimalDjinn <archiethebig@gmail.com>
2025-03-18 09:16:23 +0300

    removed range function

--- server/middleware/1.visitor.log.ts
+++ server/middleware/1.visitor.log.ts
@@ -1,16 +1,6 @@
-import type { H3Event } from "h3"
-import {consola} from "consola"
+import { consola } from "consola";
 
-function announcer() {
-    let time_start: number;
-    return {
-        handler(context: H3Event) {
-            time_start = performance.now()
-        },
-        onBeforeResponse(context: H3Event) {
-            if (!isVercel) consola.info(`[${context.node.req.method}]\t${context.node.req.url} - ${(performance.now() - time_start).toLocaleString()}ms`)
-        }
-    }
-}
-
-export default defineEventHandler(announcer())
+export default defineEventHandler((context) => {
+  if (!isVercel)
+    consola.info(`[${context.node.req.method}]   ${context.node.req.url}`);
+});
--- server/utils/auth.ts
+++
@@ -1,3 +0,0 @@
-export const LOGIN_URL = "/auth/_login"
-export const LOGOUT_URL = "/auth/_logout"
-export const USER_URL = "/auth/_user"
