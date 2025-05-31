2f8dda43041cb7a4de4fb278849c89d7fb3a3bc0
PrimalDjinn <archiethebig@gmail.com>
2025-05-31 20:59:13 +0300

    removed tailwind module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 const cwd = dirname(fileURLToPath(import.meta.url));
 
@@ -22,5 +23,9 @@ export default defineNuxtConfig({
     imports: {
       dirs: [join(cwd, "./shared/utils"), join(cwd, "./shared/types")],
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
