76303971534491b74c87856b0c973a80a227572b
PrimalDjinn <>
2025-06-10 21:32:06 +0300

    why do I keep getting seconds wrong?

--- shared/utils/time.ts
+++ shared/utils/time.ts
@@ -1,12 +1,12 @@
 export function SECONDS(time: number, unit: "hr" | "min" | "ms") {
   switch (unit) {
     case "hr":
-      return 60 * 60 * 60 * time;
+      return time * 60 * 60; // 1 hour = 3600 seconds
     case "min":
-      return 60 * 60 * time;
+      return time * 60; // 1 minute = 60 seconds
     case "ms":
-      return time / 100;
+      return time / 1000; // 1 millisecond = 1/1000 seconds
     default:
-      throw new Error("No time input unit specified");
+      throw new Error("Invalid time unit specified");
   }
 }
