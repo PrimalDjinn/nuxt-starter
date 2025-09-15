7213b87410a59aaae9dce1ba70e47c74d2486581
PrimalDjinn <archiethebig@gmail.com>
2025-09-15 20:13:13 +0300

    updates

--- shared/utils/execute.ts
+++ shared/utils/execute.ts
@@ -8,22 +8,32 @@ type Result<T, E = Error | CatchError> =
   | { result: T; error: undefined }
   | {
       result: undefined;
-      error: E extends CatchError ? ResultError<Parameters<Parameters<E>[0]>[0]> : E;
+      error: E extends CatchError
+        ? ResultError<Parameters<Parameters<E>[0]>[0]>
+        : E;
     };
 
 // Recursive type to unwrap nested promises
 type UnwrapPromise<T> = T extends Promise<infer U> ? UnwrapPromise<U> : T;
 
-type PromiseResult<T, E> = Promise<Result<UnwrapPromise<T>, T extends Promise<any> ? T["catch"] : E>>;
+type PromiseResult<T, E> = Promise<
+  Result<UnwrapPromise<T>, T extends Promise<any> ? T["catch"] : E>
+>;
 
 export function execute<F extends (...args: any[]) => any, E = Error>(
-  fun: F,
+  fun: F | None,
   ...args: Parameters<F>
-): ReturnType<F> extends Promise<any> ? PromiseResult<ReturnType<F>, E> : Result<ReturnType<F>, E>;
-export async function execute<T, E = Error>(promise: Promise<T>): PromiseResult<Promise<T>, E>;
+): ReturnType<F> extends Promise<any>
+  ? PromiseResult<ReturnType<F>, E>
+  : Result<ReturnType<F>, E>;
+export async function execute<T, E = Error>(
+  promise: Promise<T>
+): PromiseResult<Promise<T>, E>;
 export function execute(arg: any, ...args: any[]) {
   if (isPromise(arg)) {
-    return arg.then((v) => ({ result: v, error: undefined })).catch((e) => ({ result: undefined, error: e }));
+    return arg
+      .then((v) => ({ result: v, error: undefined }))
+      .catch((e) => ({ result: undefined, error: e }));
   }
 
   if (typeof arg === "function") {
